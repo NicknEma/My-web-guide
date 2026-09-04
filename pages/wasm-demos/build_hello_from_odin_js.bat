3cea5bd2736c947d7511d8f39db0316ef6dd479d
NicknEma <62065135+NicknEma@users.noreply.github.com>
2026-09-04 15:13:27 +0200

    Clean up demos

--- pages/wasm-demos/build_hello_from_odin_js.bat
+++
@@ -1,1 +0,0 @@
-odin build hello_from_odin_js.odin -file -target:js_wasm32 -out:build/hello_from_odin_js.wasm -no-entry-point
--- pages/wasm-demos/hello_from_odin_js.odin
+++
@@ -1,6 +0,0 @@
-package hello_from_odin_js
-
-@export
-addTwo :: proc "c" (a, b: i32) -> i32 {
-	return a + b
-}
